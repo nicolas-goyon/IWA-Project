b07284233420454c918539fdfa24f397675e46a2
Nicolas Goyon <nico_goyon@orange.fr>
2024-11-19 11:05:47 +0100

    add ms-location

--- ms_reservation/build.gradle.kts
+++ ms_reservation/build.gradle.kts
@@ -21,8 +21,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.5")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
 }
 
 tasks.withType<Test> {
