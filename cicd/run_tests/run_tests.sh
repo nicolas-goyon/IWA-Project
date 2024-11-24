64324ac971be891170811cecc4af264e89e175ea
Nicolas Goyon <nico_goyon@orange.fr>
2024-11-24 12:08:41 +0100

    Trap error codes

--- cicd/run_tests/run_tests.sh
+++ cicd/run_tests/run_tests.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-# run_tests.sh
+
+# Create trap to catch errors and print the line number
+cleanup() {
+    TEST_EXIT_CODE=$?
+  
+    # Create the destination directory if it doesn't exist
+    mkdir -p $DEST_DIR
+
+    # Move the reports
+    mv $SOURCE_DIR/* $DEST_DIR/
+
+    ls -la $DEST_DIR
+
+    # Check the exit code and print the appropriate message
+    if [ $TEST_EXIT_CODE -ne 0 ]; then
+    echo "Tests failed with exit code $TEST_EXIT_CODE"
+    exit $TEST_EXIT_CODE
+    else
+    echo "Tests passed and reports moved to $DEST_DIR"
+    exit 0
+    fi
+}
+
+trap 'cleanup' ERR
+
 
 # Define the source and destination directories
 SOURCE_DIR="/app/build/reports"
@@ -9,19 +33,6 @@ DEST_DIR="/app/reports"
 ./gradlew clean test --no-daemon --scan --stacktrace
 TEST_EXIT_CODE=$?
 
-# Create the destination directory if it doesn't exist
-mkdir -p $DEST_DIR
+# If no errors, run cleanup
 
-# Move the reports
-mv $SOURCE_DIR/* $DEST_DIR/
-
-ls -la $DEST_DIR
-
-# Check the exit code and print the appropriate message
-if [ $TEST_EXIT_CODE -ne 0 ]; then
-  echo "Tests failed with exit code $TEST_EXIT_CODE"
-  exit $TEST_EXIT_CODE
-else
-  echo "Tests passed and reports moved to $DEST_DIR"
-  exit 0
-fi
+cleanup $TEST_EXIT_CODE
