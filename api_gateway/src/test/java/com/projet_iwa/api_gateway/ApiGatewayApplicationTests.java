e51179cb524bea6dbf2e81322b8529745740eea0
Nicolas Goyon <nico_goyon@orange.fr>
2024-11-25 11:08:10 +0100

    Update gitignore and db build

--- api_gateway/src/test/java/com/projet_iwa/api_gateway/ApiGatewayApplicationTests.java
+++ api_gateway/src/test/java/com/projet_iwa/api_gateway/ApiGatewayApplicationTests.java
@@ -3,24 +3,18 @@ package com.projet_iwa.api_gateway;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.Duration;
 
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiGatewayApplicationTests {
 
+    @Autowired
     private WebTestClient webTestClient;
-
-    @BeforeEach
-    void setUp() {
-        webTestClient = WebTestClient.bindToServer()
-                .configureClient()
-                .baseUrl("http://localhost:8080")
-                .responseTimeout(Duration.ofSeconds(180))
-                .build();
-    }
 
     @Test
     void sendRequestToDebugService() {
--- ms_notification/database/notification.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE notification (
-  id SERIAL PRIMARY KEY,
-  icon TEXT NOT NULL,
-  title TEXT NOT NULL,
-  iduser INT NOT NULL,
-  redirection TEXT NOT NULL
-);
